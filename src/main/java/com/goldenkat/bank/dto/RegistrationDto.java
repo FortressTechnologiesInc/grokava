d10f58b50adcaac578bde3143d1eb23b98e4ecfc
FortressTechnologiesInc <130385349+FortressTechnologiesInc@users.noreply.github.com>
2026-02-17 10:54:29 -0500

    Create RegistrationDto.java

---
+++ src/main/java/com/goldenkat/bank/dto/RegistrationDto.java
@@ -0,0 +1,27 @@
+package com.goldenkat.bank.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+@Data
+public class RegistrationDto {
+    @NotBlank
+    private String firstName;
+
+    @NotBlank
+    private String lastName;
+
+    @NotBlank
+    @Email
+    private String email;
+
+    @NotBlank
+    @Size(min = 4)
+    private String username;
+
+    @NotBlank
+    @Size(min = 6)
+    private String password;
+}
