d2feb7f6497e85f879f89b7db4ba5363f2e47393
FortressTechnologiesInc <130385349+FortressTechnologiesInc@users.noreply.github.com>
2026-02-17 10:55:25 -0500

    Create TransferDto.java

---
+++ src/main/java/com/goldenkat/bank/dto/TransferDto.java
@@ -0,0 +1,23 @@
+package com.goldenkat.bank.dto;
+
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+
+import java.math.BigDecimal;
+
+@Data
+public class TransferDto {
+    @NotBlank
+    private String fromAccountNumber;
+
+    @NotBlank
+    private String toAccountNumber;
+
+    @NotNull
+    @DecimalMin("0.01")
+    private BigDecimal amount;
+
+    private String description;
+}
