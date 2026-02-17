a660bfe2e2ab77bb8e950c3ea67cd7ec97269860
FortressTechnologiesInc <130385349+FortressTechnologiesInc@users.noreply.github.com>
2026-02-17 10:58:54 -0500

    Create AccountRepository.java

---
+++ src/main/java/com/goldenkat/bank/repository/AccountRepository.java
@@ -0,0 +1,12 @@
+package com.goldenkat.bank.repository;
+
+import com.goldenkat.bank.model.Account;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface AccountRepository extends JpaRepository<Account, Long> {
+    List<Account> findByUserId(Long userId);
+    Optional<Account> findByAccountNumber(String accountNumber);
+}
